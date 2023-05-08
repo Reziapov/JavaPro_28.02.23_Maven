3db6fdbf7cc29e55bf928479dbfa579c225f20af
Reziapov Artur <areziapov@gmail.com>
2023-05-08 19:52:24 +0300

    homework 14 (calculator consol)

--- src/main/java/com/hillel/reziapov/homeworks/homework14/Main.java
+++ src/main/java/com/hillel/reziapov/homeworks/homework14/Main.java
@@ -1,7 +1,40 @@
 package com.hillel.reziapov.homeworks.homework14;
 
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        if (args.length != 3) {
+            System.out.println("Enter: Calculator <number1> <number2> <operator>");
+            return;
+        }
+
+        double number1 = Double.parseDouble(args[0]);
+        double number2 = Double.parseDouble(args[1]);
+        String operator = args[2];
+        double result;
+
+            switch (operator) {
+                case "+" -> result = number1 + number2;
+                case "-" -> result = number1 - number2;
+                case "x" -> result = number1 * number2;
+                case "/" -> {
+                    if (number2 == 0) {
+                        System.out.println("Cannot divide by zero!");
+                        return;
+                    }
+                    result = number1 / number2;
+                }
+                default -> {
+                    System.out.println("Incorrect operator! Please restart ");
+                    return;
+                }
+            }
+            System.out.println(number1 + " " + operator + " " + number2 + " = " + result);
+
+
+        }
+
     }
-}
+
+
